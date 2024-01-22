5b116f0260d55a1c37424d34f8d7336b2dc24031
Bruno Xavier <bruno@brunoxavier.com.br>
2024-01-22 18:27:37 -0300

    seller dao jdbc factory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -5,15 +5,10 @@ import model.entities.Department;
 import java.util.List;
 
 public interface DepartmentDao {
-
     void insert(Department obj);
-
     void update(Department obj);
-
     void deleteById(Integer id);
-
     Department findById(Integer id);
-
     List<Department> finAll();
 
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,19 +7,10 @@ import java.util.List;
 
 public interface SellerDao {
         void insert(Seller obj);
-
         void update(Seller obj);
-
         void deleteById(Integer id);
-
         Seller findById(Integer id);
-
         List<Seller> finAll();
-
-    }
-
-
-
 }
 
 
