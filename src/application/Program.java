cfaa53e843415c6e40b26faad27cd1e7a577cdc3
Bruno Xavier <bruno@brunoxavier.com.br>
2024-01-22 20:17:13 -0300

    find by department dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,16 +5,24 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("---- Test 1 - Find Seller By Id ----");
+        System.out.println("---- Test 1 - findById ----");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n---- Test 1 - findByDepartment ----");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
 
     }
 }
